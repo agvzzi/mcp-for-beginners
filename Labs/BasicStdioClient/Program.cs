eae0544f032bf1cb3cf436d5fe35dfba3f0f1753
Alberto Aguzzi <alberto.aguzzi@macaw.nl>
2025-11-24 15:34:05 +0100

    Refactor to integrate MCP client and tool handling
    Replaced the default "Hello, World!" application with an MCP client implementation. Added support for listing available tools and invoking the `get_greeting` tool with a parameter. Updated the `StdioClientTransport` configuration to use a pre-built executable for the server. Introduced `Host.CreateApplicationBuilder` for application configuration, including environment variables and user secrets.

--- Labs/BasicStdioClient/Program.cs
+++ Labs/BasicStdioClient/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>();
+
+//var clientTransport = new StdioClientTransport(new()
+//{
+//    Name = "Basic Stdio Server",
+//    Command = "dotnet",
+//    Arguments = ["run", "--project", "..\\..\\..\\..\\BasicStdioServer\\BasicStdioServer.csproj"],
+//});
+var clientTransport = new StdioClientTransport(new()
+{
+    Name = "Basic Stdio Server",
+    Command = "..\\..\\..\\..\\BasicStdioServer\\bin\\Debug\\net9.0\\BasicStdioServer.exe"
+});
+
+await using var mcpClient = await McpClient.CreateAsync(clientTransport);
+
+foreach (var tool in await mcpClient.ListToolsAsync())
+{
+    Console.WriteLine($"{tool.Name} ({tool.Description})");
+}
+
+var result = await mcpClient.CallToolAsync(
+    "get_greeting",
+    new Dictionary<string, object?>() { ["name"] = "Alberto Aguzzi"},
+    cancellationToken: CancellationToken.None);
+
+
+if (result.Content.FirstOrDefault(c => c.Type == "text") is TextContentBlock textBlock && textBlock.Text is not null)
+{
+    Console.WriteLine(textBlock.Text);
+}
+else
+{
+    Console.WriteLine("<no text>");
+}
